726838736e59315a614590aa7efe727187db4da8
Stéphane Kermabon <stephane.kermabon.partner@decathlon.com>
2024-06-10 14:20:34 +0200

    Live Coding

---
+++ src/test/groovy/org/demis27/exception/LiveExceptionSpec.groovy
@@ -0,0 +1,16 @@
+package org.demis27.exception
+
+import org.demis27.Fibonacci
+import org.demis27.FibonacciException
+import org.demis27.FibonacciSequence
+import org.demis27.FibonacciSequenceException
+import spock.lang.Specification
+
+class LiveExceptionSpec extends Specification{
+
+    def 'test exception' () {
+    }
+
+    def 'test exception sequence' () {
+    }
+}
---
+++ src/test/groovy/org/demis27/stubMockAndSpy/LiveStubSequenceSpec.groovy
@@ -0,0 +1,12 @@
+package org.demis27.stubMockAndSpy
+
+import org.demis27.Fibonacci
+import org.demis27.FibonacciSequence
+import spock.lang.Specification
+
+class LiveStubSequenceSpec extends Specification {
+
+
+    def 'test fibonacci sequence stub'() {
+    }
+}
